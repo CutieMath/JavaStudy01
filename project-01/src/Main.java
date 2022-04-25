714914100702ff2720ef395cb14630b2afa36578
CutieMath <cutie.math@protonmail.com>
2022-04-25 14:21:32 +1000

    format my assignmebt

--- project-01/src/Main.java
+++ project-01/src/Main.java
@@ -4,31 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-            final byte MONTHS_IN_YEAR = 12;
-            final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-        try{
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            // Take input
-            System.out.print("Principle: ");
-            int principle = scanner.nextInt();
-            System.out.print("Annual Interest Rate: ");
-            float interestRate = scanner.nextFloat();
-            System.out.print("Period (Years): ");
-            short period = scanner.nextShort();
+        // Take input
+        System.out.print("Principle: ");
+        int principle = scanner.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float interestRate = scanner.nextFloat();
+        System.out.print("Period (Years): ");
+        short period = scanner.nextShort();
 
-            // Calculate the result
-            interestRate = (interestRate / PERCENT) / MONTHS_IN_YEAR;
-            period *= 12;
-            double resRaw = principle * ((interestRate * Math.pow((1 + interestRate), period)) / (Math.pow((1 + interestRate), period) - 1));
+        // Calculate the result
+        interestRate = (interestRate / PERCENT) / MONTHS_IN_YEAR;
+        period *= 12;
+        double resRaw = principle * ((interestRate * Math.pow((1 + interestRate), period)) / (Math.pow((1 + interestRate), period) - 1));
 
-            // Format the result and print
-            NumberFormat formatter = NumberFormat.getCurrencyInstance();
-            System.out.print("Your monthly payment is: " + formatter.format(resRaw));
+        // Format the result and print
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        System.out.print("Your monthly payment is: " + formatter.format(resRaw));
 
-        } catch(Exception e) {
-            System.out.println("Error occurred:" + e.toString());
-        }
     }
 }
