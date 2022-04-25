ffb7faa93feca7386b148285e398e7281667b90a
CutieMath <cutie.math@protonmail.com>
2022-04-25 17:29:04 +1000

    add input value validation

--- project-01/src/Main.java
+++ project-01/src/Main.java
@@ -3,19 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
 
         // Take input
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float interestRate = scanner.nextFloat();
-        System.out.print("Period (Years): ");
-        short period = scanner.nextShort();
+        int principle = 0;
+        System.out.print("Principle ($1K - $1M): ");
+        principle = scanner.nextInt();
+        while(principle < 1000 || principle > 1_000_000_000){
+            System.out.println("Please enter value between $1K - $1M.");
+            System.out.print("Principle ($1K - $1M): ");
+            principle = scanner.nextInt();
+        }
+
+        float interestRate = (float)0.00;
+        System.out.print("Annual Interest Rate (0 - 30): ");
+        interestRate = scanner.nextFloat();
+        while(interestRate <= 0 || interestRate > 30) {
+            System.out.println("Please enter value between 0 - 30.");
+            System.out.print("Annual Interest Rate (0 - 30): ");
+            interestRate = scanner.nextFloat();
+        }
+
+        short period = 0;
+        System.out.print("Period (Years, 1 - 30): ");
+        period = scanner.nextShort();
+        while(period < 1 || period > 30){
+            System.out.println("Please enter value between 1 - 30.");
+            System.out.print("Period (Years, 1- 30): ");
+            period = scanner.nextShort();
+        }
 
         // Calculate the result
         interestRate = (interestRate / PERCENT) / MONTHS_IN_YEAR;
@@ -25,6 +44,5 @@ public class Main {
         // Format the result and print
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         System.out.print("Your monthly payment is: " + formatter.format(resRaw));
-
     }
 }
