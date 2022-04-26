c58965363a2314f226669f9efee0a7a067fad5cb
CutieMath <cutie.math@protonmail.com>
2022-04-26 19:21:49 +1000

    encapsulate the value inside classes

--- project-02/src/com/cutieMath02/Employee.java
+++ project-02/src/com/cutieMath02/Employee.java
@@ -1,4 +1,13 @@
 package com.cutieMath02;
 
 public class Employee {
+    private int baseSalary;
+    private int hourlyRage;
+    private int extraHours;
+
+    public int calculateWage(){
+        return baseSalary + hourlyRage * extraHours;
+    }
+
+
 }
