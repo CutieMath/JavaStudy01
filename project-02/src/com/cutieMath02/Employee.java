c7117cc2b56eea6b2879f47045133b7274d14990
CutieMath <cutie.math@protonmail.com>
2022-04-26 19:44:42 +1000

    use getter and setter

--- project-02/src/com/cutieMath02/Employee.java
+++ project-02/src/com/cutieMath02/Employee.java
@@ -6,10 +6,24 @@ public class Employee {
 
     // Setter
     public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0){
+            throw new IllegalArgumentException("Base salary cannot be zero or less.");
+        }
         this.baseSalary = baseSalary;
     }
     public void setHourlyRage(int hourlyRage) {
+        if (hourlyRage <= 0){
+            throw new IllegalArgumentException("Hourly rate cannot be zero or less.");
+        }
         this.hourlyRage = hourlyRage;
+    }
+
+    // Getter
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+    public int getHourlyRage() {
+        return hourlyRage;
     }
 
     // Methods
--- project-02/src/com/cutieMath02/Main.java
+++ project-02/src/com/cutieMath02/Main.java
@@ -3,11 +3,10 @@ package com.cutieMath02;
 public class Main {
     public static void main(String[] args){
         Employee employee = new Employee();
-        employee.setBaseSalary(50_000);
+        employee.setBaseSalary(5_000);
         employee.setHourlyRage(20);
         int extraHours = 10;
         int wage = employee.calculateWage(extraHours);
         System.out.println(wage);
     }
-
 }
